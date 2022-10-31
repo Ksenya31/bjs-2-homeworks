91008c7874ddc14fe07b5da34196f8e82946e47d
ksenya31 <kseniakalinnikova268@gmail.com>
2022-10-31 19:03:00 +0300

    change fail

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,23 @@
+"use strict"
+  // Задача №1
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b**2-4*a*c;
+  if (d > 0) {
+    console.log(`Дискриминант ${d} > 0, 2 корня`);
+    let x1 = (-b + Math.sqrt(d))/(2*a);
+    let x2 = (-b - Math.sqrt(d))/(2*a);
+    arr.push(x1, x2);
+  } else if (d == 0) {
+    console.log(`Дискриминант ${d} = 0, 1 корень`);
+      let x1 = -b/(2*a);
+      arr.push(x1);
+  }      else if (d < 0) {
+        console.log(`Дискриминант ${d} < 0, корней нет`);
+  }
+   console.log(arr);
+  return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
