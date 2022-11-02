04440f8f33961a5ad4748dc5762f70658e21da9b
ksenya31 <kseniakalinnikova268@gmail.com>
2022-11-02 16:16:44 +0300

    added task 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,5 +19,29 @@ function solveEquation(a, b, c) {
   return arr;
 }
 
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let p = parseInt(percent) / 100 / 12; //процентная ставка
+    let c = parseInt(contribution);     //начальный взнос 
+    let a = parseInt(amount);         //общая стоимость 
+    if (isNaN(p) || p < 0) {
+      return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    } else if (isNaN(c) || c < 0) {
+      return `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
+    } else if (isNaN(a) || a < 0) {
+      return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
+    } else {
 
+      let today = new Date();
+      if (today.getFullYear() > date.getFullYear()) {
+      return `Параметр "срок ипотеки" содержит неправильное значение ${date}`;
+    } else {
+      let s = a - c;              //тело кредита
+        let n = date.getMonth() - today.getMonth() + (12*(date.getFullYear() - today.getFullYear())); //срок кредита в месяцах
+        let pay = s*(p+(p/(((1+p)**n)-1)));     //ежемесячная оплата
+        let totalAmount = (pay * n).toFixed(2);
+        console.log(`Общая сумма по ипотеке составляет ${totalAmount}`);
+      return +totalAmount;
+    }
+  }
+}
 
