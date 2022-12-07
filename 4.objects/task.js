40fd36845ba6d339d8d32a869a5978090b9ae023
ksenya31 <kseniakalinnikova268@gmail.com>
2022-12-07 18:49:11 +0300

    added objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,48 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+}
+  const student1 = new Student("Chester", "male", 41);
+  const student2 = new Student("Mike", "male", 19);
+  const student3 = new Student("Rob", "female", 20);
 
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined){
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function(...spr) {
+  if(this.marks === undefined){
+    this.marks = spr;
+  } else {
+    this.marks.push(...spr);
+  }
+}
+
+Student.prototype.getAverage = function() {
+  if(this.marks.length === 0){
+    return 0;
+  } else {
+    let sum = 0;
+    let count = 0;
+      for (let i = 0; i < this.marks.length; i++){
+       count += 1;
+       sum += this.marks[i];
+      }
+    return sum/count
+  }
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
